f94cd9fbbd49682f6c4c868f881408f669479df3
Jaybee Sarmiento <jaybesarmiento@gmail.com>
2019-09-03 09:02:57 +0800

    added redis_test.php

---
+++ redis_test.php
@@ -0,0 +1,11 @@
+<?php
+   $host = $_GET['redis_host'];
+   if(empty($host)) {
+     echo "No Host : (http://url?redis_host=<redis_host>)";
+     die();
+   }
+   $redis = new Redis(); 
+   $redis->connect($host, 6379); 
+   echo "Connection to server sucessfully"; 
+   //check whether server is running or not 
+   echo "Server is running: ".$redis->ping();
